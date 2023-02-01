78d818ecf86633cb19dbda40021373989d0c36dc
Your Name <you@example.com>
2023-01-31 22:34:46 -0600

    RETO 1

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,54 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int operationIndex = 0;
-        do {
-            String operation = args[operationIndex];
-            int parameter1 = Integer.parseInt(args[operationIndex + 1]);
-            int parameter2 = Integer.parseInt(args[operationIndex + 2]);
+        Scanner scan = new Scanner(System.in);
+        String number1;
+        String number2;
 
-            switch (operation) {
-                case "SUMA" ->
-                        System.out.println(parameter1 + " + " + parameter2 + " = " + (parameter1 + parameter2));
-                case "RESTA" ->
-                        System.out.println(parameter1 + " - " + parameter2 + " = " + (parameter1 - parameter2));
-                case "MULTIPLICACION" ->
-                        System.out.println(parameter1 + " * " + parameter2 + " = " + (parameter1 * parameter2));
-                case "DIVISION" ->
-                        System.out.println(parameter1 + " / " + parameter2 + " = " + (parameter1 / parameter2));
-            }
-            operationIndex += 3;
-        }while(operationIndex < args.length);
+        System.out.print("Captura Operación: ");
+        String operation = scan.nextLine();
+
+        switch (operation) {
+            case "SUMA":
+                System.out.print("Operando 1: ");
+                number1 = scan.nextLine();
+                int operando1 = Integer.parseInt(number1);
+                System.out.print("Operando 2: ");
+                number2 = scan.nextLine();
+                int operando2 = Integer.parseInt(number2);
+                System.out.println(operando1 + " + " + operando2 + " = " + (operando1+operando2));
+                break;
+            case "RESTA":
+                System.out.print("Operando 1: ");
+                number1 = scan.nextLine();
+                int operando3 = Integer.parseInt(number1);
+                System.out.print("Operando 2: ");
+                number2 = scan.nextLine();
+                int operando4 = Integer.parseInt(number2);
+                System.out.println(operando3 + " - " + operando4 + " = " + (operando3-operando4));
+                break;
+            case "MULTIPLICACION":
+                System.out.print("Operando 1: ");
+                number1 = scan.nextLine();
+                int operando5 = Integer.parseInt(number1);
+                System.out.print("Operando 2: ");
+                number2 = scan.nextLine();
+                int operando6 = Integer.parseInt(number2);
+                System.out.println(operando5 + " * " + operando6 + " = " + (operando5*operando6));
+                break;
+            case "DIVISION":
+                System.out.print("Operando 1: ");
+                number1 = scan.nextLine();
+                int operando7 = Integer.parseInt(number1);
+                System.out.print("Operando 2: ");
+                number2 = scan.nextLine();
+                int operando8 = Integer.parseInt(number2);
+                System.out.println(operando7 + " / " + operando8 + " = " + (operando7/operando8));
+                break;
+            default:
+                System.out.println("Operación no soportada");
+                break;
+        }
     }
 }
